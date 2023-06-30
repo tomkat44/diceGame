7f77cdbf7d7f5eeb2a6cb3f4ed3247c748ecce99
ObserverOfTime <chronobserver@disroot.org>
2023-06-30 23:18:33 +0300

    Maintain the theme across pages

--- frontend/theme.js
+++ frontend/theme.js
@@ -1,8 +1,12 @@
+document.documentElement.dataset.theme = sessionStorage.theme || 'dark';
+
 window.matchMedia('(prefers-color-scheme: light)').addEventListener('change', (evt) => {
-    document.documentElement.dataset.theme = evt.matches ? 'light' : 'dark';
+    sessionStorage.theme = document.documentElement.dataset.theme = evt.matches ? 'light' : 'dark';
 });
 
-document.getElementById('switch-theme').addEventListener('click', () => {
-    const dataset = document.documentElement.dataset;
-    dataset.theme = dataset.theme == 'dark' ? 'light' : 'dark';
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('switch-theme').addEventListener('click', () => {
+        const dataset = document.documentElement.dataset;
+        sessionStorage.theme = dataset.theme = dataset.theme == 'dark' ? 'light' : 'dark';
+    });
 });
